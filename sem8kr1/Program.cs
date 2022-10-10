9ce0633f7c3b7614848b06388c0f72d5f10b683d
evgenii <evgenii.lyabov@mail.ru>
2022-10-10 20:48:08 +0300

    add sem8kr1

---
+++ sem8kr1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, 
+/*которая заменяет строки на столбцы. В случае, если это невозможно, 
+программа должна вывести сообщение для пользователя.
+Исходный массив:		Результат замены:
+9 2 3          			9 4 2			
+4 2 4          			2 2 6
+2 6 7 				        3 4 7*/
+
+Console.Clear();
+int arrayRangeMin = -10;
+int arrayRangeMax = 10;
+
+Console.WriteLine("Создаем новый массив.");
+int rows = UserInput("\nВведите количество строк: ");
+int columns = UserInput("Введите количество столбцов: ");
+
+int[,] array = new int[rows, columns];
+
+CreateArray(array);
+WriteArray(array);
+
+int[,] arrayTrans = new int[columns, rows];
+
+
+try
+{
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            arrayTrans[i, j] = array[j, i];
+        }
+    }
+    Console.WriteLine("\n Создаем транспонированый массив: \n");
+
+    WriteArray(arrayTrans);
+}
+
+catch
+{
+    Console.WriteLine("\n Невозможно транспонировать массив. \n");
+}
